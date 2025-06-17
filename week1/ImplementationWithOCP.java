a9604f4e17cf9e713f03b0d940cfed4ae5803578
Shailendra Pathak <153452113+PathakShailendra@users.noreply.github.com>
2025-06-17 19:11:58 +0530

    Completed Day 2 - OCP (Open/Closed Principle) with interface-based extensible design

---
+++ week1/ImplementationWithOCP.java
@@ -0,0 +1,48 @@
+package week1;
+
+public class ImplementationWithOCP {
+    public interface Payment {
+        void pay();
+    }
+
+    static class CreditCardPayment implements Payment {
+        public void pay() {
+            System.out.println("Processing Credit Card Payment...");
+        }
+    }
+
+    static class DebitCardPayment implements Payment {
+        public void pay() {
+            System.out.println("Processing Debit Card Payment...");
+        }
+    }
+
+    static class UpiPayment implements Payment {
+        public void pay() {
+            System.out.println("Processing UPI Payment...");
+        }
+    }
+
+    static class PaymentProcessor {
+
+        public void makePayment(Payment payment) {
+            payment.pay();
+        }
+    }
+
+
+        public static void main(String[] args) {
+
+            PaymentProcessor processor = new PaymentProcessor();
+
+            Payment credit = new CreditCardPayment();
+            Payment debit = new DebitCardPayment();
+            Payment upi = new UpiPayment();
+
+            processor.makePayment(credit);
+            processor.makePayment(debit);
+            processor.makePayment(upi);
+        }
+
+
+}
