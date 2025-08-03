5c0ca0eaf974acf68a569790c6a98a7b9d9873e6
Shailendra Pathak <153452113+PathakShailendra@users.noreply.github.com>
2025-08-03 21:55:33 +0530

    Delete week7/13. ReactJS-HOL directory

--- week7/13. ReactJS-HOL/bloggerapp/src/data/blogs.js
+++
@@ -1,4 +0,0 @@
-export const blogs = [
-  { title: 'React Learning', author: 'Stephen Biz', content: 'Welcome to learning React!' },
-  { title: 'Installation', author: 'Schwezdenier', content: 'You can install React from npm.' },
-];
--- week7/13. ReactJS-HOL/bloggerapp/src/data/courses.js
+++
@@ -1,4 +0,0 @@
-export const courses = [
-  { name: 'Angular', date: '4/5/2021' },
-  { name: 'React', date: '6/3/2021' },
-];
