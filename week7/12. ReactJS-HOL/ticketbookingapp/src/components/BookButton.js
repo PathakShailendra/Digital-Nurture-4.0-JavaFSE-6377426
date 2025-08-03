d65a647a92e8ccdb1de560b3ebf845f93efa0a33
Shailendra Pathak <153452113+PathakShailendra@users.noreply.github.com>
2025-08-03 21:55:15 +0530

    Delete week7/12. ReactJS-HOL directory

--- week7/12. ReactJS-HOL/ticketbookingapp/src/components/BookButton.js
+++
@@ -1,9 +0,0 @@
-// src/components/BookButton.js
-import React from 'react';
-
-const BookButton = ({ isLoggedIn }) => {
-  if (!isLoggedIn) return null;
-  return <button>Book</button>;
-};
-
-export default BookButton;
